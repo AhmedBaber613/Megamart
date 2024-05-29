fd243ec7ddcf886667048502ef3f720a8c69ec65
Ahmed Baber <145803188+AhmedBaber613@users.noreply.github.com>
2024-05-29 22:37:01 +0500

    more bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -21,10 +21,6 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
       }
     ]
   }
